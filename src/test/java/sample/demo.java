bc3fc6406903641fe6572573a64fa7e03e006725
PiyushaChipare <pushachip@gmail.com>
2023-04-12 14:19:06 +0530

    1st Demo

---
+++ src/test/java/sample/demo.java
@@ -0,0 +1,14 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class demo 
+{
+	@Test
+	public void demo() 
+	{
+		System.out.println("This is demo");
+		System.out.println("hi");
+		System.out.println("hi");
+	}
+}
